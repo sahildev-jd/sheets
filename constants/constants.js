740496b70dffbbaa3c92d2aff3fb6bf001ad6f17
Sahil Dev <sahil.dev@vedantu.com>
2022-04-16 13:47:36 +0530

    default no of rows and columns

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
-export const NO_OF_ROWS = 5;
-export const NO_OF_COLUMNS = 5;
+export const NO_OF_ROWS = 20;
+export const NO_OF_COLUMNS = 20;
 export const ROW = "row";
 export const COLUMN = "column";
 export const PLUS_ONE = "+1";
